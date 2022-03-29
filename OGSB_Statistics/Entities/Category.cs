dedf427ce9a953534a9b58b87725072c6ae7119c
SolaAkinwale <sakinwale@gmail.com>
2022-03-29 14:06:41 +0100

    Add project files.

---
+++ OGSB_Statistics/Entities/Category.cs
@@ -0,0 +1,33 @@
+﻿using OGSB_Statistics.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OGSB_Statistics.Entities
+{
+    public class Category: IPrimaryProperties
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(200, MinimumLength =2)]
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        [Required]
+        [Display(Name = "Thumbnail Image Path")]
+        public string ThumbnailImagePath { get; set; }
+
+        [ForeignKey("CategoryId")]
+        public virtual ICollection<CategoryItem> CategoryItems { get; set; }
+
+        [ForeignKey("CategoryId")]
+        public virtual ICollection<UserCategory> UserCategory { get; set; }
+
+
+
+    }
+}
---
+++ OGSB_Statistics/Models/RegistrationModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OGSB_Statistics.Models
+{
+    public class RegistrationModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "User Name")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 2)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 2)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [RegularExpression(@"(\s*\(?0\d{4}\)?\s*\d{6}\s*)|(\s*\(?0\d{3}\)?\s*\d{3}\s*\d{4}\s*)")]
+        [Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [Display(Name = "Organization")]
+        public string Organization { get; set; }
+
+        [Required]
+        [Display(Name = "Street")]
+        public string Address1 { get; set; }
+
+        [Required]
+        [Display(Name = "Town/State")]
+        public string Address2 { get; set; }
+
+        public bool AcceptUserAgreement { get; set; }
+        public string RegistrationInValid { get; set; }
+        //public int CategoryId { get; set; }
+    }
+}
